ac962ce7878530ffa31c7df8872e4d73b26b55fd
yos <mayra15.yos@gmail.com>
2024-06-19 21:27:46 -0600

    first commit

--- index.php
+++ index.php
@@ -1,50 +1,69 @@
-<?php
-echo "my first PHP script";
-echo "<br>";
-echo " Mis datos";
-$nombre = " Maira Joselin";
-$edad = 19;
-$estatura = 1.60;
-echo " soy...". strtoupper ($nombre). " tengo". $edad. " años y mido". $estatura. "metros";
-echo "<br>";
-var_dump(is_int($nombre));
-echo"<br>";
-
-$x = 34;
-$y = 356.78;
-$z = $x+$y;
-echo "La suma es $x + $y = $z <br>";
-echo"<br>";
-$x = 34;
-$y = 25;
-$z = $x*$y;
-echo "La multiplicacion es $x * $y = $z <br>";
-echo"<br>";
-$x = 100;
-$y = 25;
-$z = $x-$y;
-echo "La resta es $x - $y = $z <br>";
-echo"<br>";
-$x = 250;
-$y = 25;
-$z = $x/$y;
-echo "La Divicion es $x / $y = $z <br>";
-echo"<br>";
-$base = 2;
-$exponente = 3;
-$potencia = $base ** $exponente;
-echo "La potencia es $base elevado a $exponente es: $potencia";
-echo "<br>";
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>PRACTICA PHP</title>
+    <title>Conversor Decimal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    HOLA!!
+    <div align='center'>
+    <h1 style="color:#C8020B;" >Conversion Decimal</h1>
+    <form method="post">
+        <label for="opcion">Tipo de conversión:</label>
+        <select name="opcion" id="opcion">
+            <option value="binario">Decimal a Binario</option>
+            <option value="octal">Decimal a Octal</option>
+            <option value="hexadecimal">Decimal a Hexadecimal</option>
+        </select>
+        <br><br>
+        <label for="numero">Ingresa el número decimal:</label>
+        <input type="text" name="numero" id="numero">
+        <br><br>
+        <button type="submit" value="convertir" class="btn btn-outline-danger">convertir</button>
+    </form>
+    
+
+
+    
+    <?php
+    
+    function decimal_a_binario($decimal) {
+        return decbin($decimal);
+    }
+    
+    
+    function decimal_a_octal($decimal) {
+        return decoct($decimal);
+    }
+    
+   
+    function decimal_a_hexadecimal($decimal) {
+        return dechex($decimal);
+    }
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $opcion = $_POST['opcion'];
+        $numero_decimal = $_POST['numero'];
+        
+        switch ($opcion) {
+            case 'binario':
+                $resultado = decimal_a_binario($numero_decimal);
+                echo "<p>El número binario equivalente es: $resultado</p>";
+                break;
+            case 'octal':
+                $resultado = decimal_a_octal($numero_decimal);
+                echo "<p>El número octal equivalente es: $resultado</p>";
+                break;
+            case 'hexadecimal':
+                $resultado = decimal_a_hexadecimal($numero_decimal);
+                echo "<p>El número hexadecimal equivalente es: $resultado</p>";
+                break;
+            default:
+                echo "<p>Opción inválida</p>";
+                break;
+        }
+    }
+    ?>
+    </div>
 </body>
 </html>
